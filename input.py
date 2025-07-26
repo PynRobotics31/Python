24fb4bcdf39a0912e3cb65f238d38feaa26a126d
PynRobotics31 <subhamkumarbetal2010@gmail.com>
2025-07-26 19:37:19 +0530

    Delete input.py

--- input.py
+++
@@ -1,7 +0,0 @@
-name = input("What is your name")
-print("hello ",name)
-
-a = int(input("Enter value of a :"))
-b= int(input("Enter value of b : "))
-print("sum of a and b is ",a+b)
-print("difference of a and b is",a-b)
