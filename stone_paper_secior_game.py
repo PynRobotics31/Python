6f5a9bd58724d72fb69ee3060bbba9947f7b370c
PynRobotics31 <subhamkumarbetal2010@gmail.com>
2025-08-12 12:07:42 +0530

    Add files via upload
    This is a cli based stone paper secior game

--- stone_paper_secior_game.py
+++ stone_paper_secior_game.py
@@ -2,21 +2,16 @@ import random
 choise = ["stone","paper","secior"]
 len = 1
 while True:
-    robo = "".join(random.sample(choise,len))
-    you = input("Enter your choise (exit for end the game) : ")
-    print("robot :",robo)
-
-    if(you == "stone" and robo=="stone" or you=="secior" and robo=="secior" or you=="paper"and robo=="paper"):
-        print("result : match draw")
-    
-    elif(you == "stone" and robo=="secior" or you=="paper" and robo=="stone" or you=="secior"and robo=="paper"):
-        print("result : you win")
-    
-    elif(you == "secior" and robo=="stone" or you=="stone" and robo=="paper" or you=="paper"and robo=="secior"):
-        print("resule : robot win")
-    
-    elif(you == "exit"):
-        print("match stoped")
-        break
-    else:
-        print("you can enter stone , paper or secior")
+	you = input("Enter your choise : ")
+	robo = "".join(random.sample(choise,len))
+	print("robot : ",robo)
+	if(you=="stone" and robo == "stone" or you=="paper" and robo=="paper" or you=="secior" and robo=="secior"):
+		print("match draw")
+		
+	elif(you =="stone" and robo=="secior" or you=="paper" and robo=="stone" or you=="secior" and robo=="paper"):
+		print("you win")
+		
+	elif(you =="secior" and robo=="stone" or you=="stone" and robo=="paper" or you=="paper" and robo=="secior"):
+		print("robot win")
+	else:
+		print("You can input only stone ,paper or secior")
