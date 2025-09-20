4430fb5680dee8edf905561917876d99c48646bd
PynRobotics31 <subhamkumarbetal2010@gmail.com>
2025-09-20 20:23:56 +0530

    adding the jarvis file

--- jarvis.py
+++ jarvis.py
@@ -4,11 +4,10 @@ import time
 import os
 import datetime
 import pyttsx3
-import pywhatkit as kit
-from deep_translator import GoogleTranslator
-import requests 
+import requests
 import json
-
+import wikipedia as wiki
+import pywhatkit as kit
 def speak(audio):
     engine = pyttsx3.init("sapi5")
     engine.say(audio)
@@ -52,6 +51,32 @@ while True:
         print("Jarvis : Opening notepad")
         speak("Opening notepad")
         os.system("notepad")
+
+    elif(a == "open excel"):
+        print("Jarvis : Opening Excel")
+        speak("Opening excel")
+        os.startfile(r"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE")
+    
+    elif(a == "open calculator"):
+        print("Jarvis : Opening calculaor")
+        speak("Opening calculaor")
+        os.startfile("calc.exe")
+    
+    elif(a == "open power point"):
+        print("Jarvis : Opening power point")
+        speak("Opening power point")
+        os.startfile(r"C:\Program Files\Microsoft Office\root\Office16\POWERPNT.EXE")
+
+    elif(a == "open vscode"):
+        print("Jarvis : Opening Vscode")
+        speak("Opening vscode")
+        os.startfile(r"C:\Users\Kushadhaj Betal\AppData\Local\Programs\Microsoft VS Code\Code.exe")
+
+    elif(a == "open word" or a == "open word 365" or a == "ms word"):
+        print("Jarvis : Opening Word 365")
+        speak("Opening Word 365")
+        os.startfile(r"C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE")
+    
     
     elif(a.startswith("open ")):
         web = a[5:]
@@ -62,13 +87,12 @@ while True:
     elif(a.startswith("tell me about ")):
         query = a[14:]
         try:
-            import wikipedia as wiki
+            
             data = wiki.summary(query)
             print(f"Jarvis : {data}")
             speak(data)
-        except Exception as e:
+        except:
             print(f"Jarvis : I am sorry but i can't give you the information about {query}")
-            print(f"The problem is {e}")
             speak(f"I am sorry but i can't give you the information about {query}")
     
     elif(a.startswith("search in youtube ")):
@@ -82,13 +106,6 @@ while True:
         print(f"Jarvis : Searching in google {sg}")
         speak(f"Searching in google {sg}")
         wb.open(f"https://www.google.com/search?q={sg}")
-    
-    elif(a.startswith("play ")):
-        song = a[5:]
-        print(f"Jarvis : Playing {song}")
-        speak(f"Playing {song}")
-        wb.open(f"https://music.youtube.com/search?q={song}")
-
     
     elif(a == "tell me a joke"):
         joke = pyjokes.get_joke()
@@ -104,14 +121,71 @@ while True:
         except:
             print("Jarvis : I can't answering you about your sum There can be some problem ")
             speak("I can't answering you about your sum There can be some problem ")
-
+        
+    elif(a.startswith("play ")):
+        song = a[5:]
+        print(f"Jarvis : Playing {song}")
+        speak(f"Plyaing {song}")
+        wb.open(f"https://music.youtube.com/search?q={song}")
+        
     elif(a == "bye" or a =="exit"):
         print("Jarvis : Ok bye have a nice day")
         speak("Ok bye have a nice day")
         break
-
-    
+    elif(a.startswith("weather report of ")):
+        temp_query=a[18:]
+        try:
+            apikey = "7040ea904442a45d6950ba584410ce59"
             
+            baseURL= "https://api.openweathermap.org/data/2.5/weather?q="
+            
+            cityname = temp_query
+            
+            completeURL = baseURL+ cityname+"&appid=" + apikey
+            response =requests.get(completeURL)
+            
+            data= response.json()
+            print(f"Jarvis : Current weather of {cityname}")
+            speak(f"Current weather of {cityname}")
+            print("Current Tempreture : ",data["main"]["temp"])    		
+            print("Current Tempreture Feels like : ",data["main"]["feels_like"])    		
+            print("Maximum Tempreture : " ,data["main"]["temp_max"]) 		
+            print("Maximum Tempreture : ",data["main"]["temp_min"])   		
+            print("Pressure: ",data["main"]["pressure"]) 		
+            print("Humidity : ",data["main"]["humidity"])		
+            print("Sea Level :",data["main"]["sea_level"])
+            print("Groud Level :",data["main"]["grnd_level"])
+    	
+        except Exception as e:
+            print(f"Jarvis : Sorry sir but i can't give you the weather report. This problem generate for {e}")
+
+    elif a.startswith("send message to "):
+        now = datetime.datetime.now()
+        hour = now.hour
+        minute = now.minute+1
+        print(hour, minute)
+        users = {
+            "shayan":"+918158098686",
+            "mom":"+919851807533",
+            "didi":"+917319085574",
+            "dad":"+919079156032",
+            "swaranya":"+919734823637",
+            "sayan purakit":"+917908036963",
+        }
+        user = a[16:]
+        if(user in users):
+            message = input("Jarvis : Enter the text to send : ")
+            phone = users[user]
+            try:
+                
+                
+                kit.sendwhatmsg(users[user],message,hour,minute)
+                print(f"Jarvis : Message Succesfully send to {user}")
+            except Exception as e:
+                print(f"Jarvis : Message can't be sended. Problem is {e}")
+
+        else:
+            print(f"Jarvis : {user} is not saved")
 
     else:
         print("Jarvis : Sorry but i don't understand your command sir")
